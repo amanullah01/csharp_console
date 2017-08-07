f7170cc7289f3ba1156ca786f05ca38d4854b5d0
Amanullah Aman <amancse.aiub@gmail.com>
2017-08-07 11:51:19 +0600

    Implecit and explicite conversion

---
+++ y_datatype_conversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace y_datatype_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implicite and Explecite conversion");
+
+            int i = 1040;
+            float f = i;
+            Console.WriteLine($"Implecite Conversion done: {f}");
+
+            float f1 = 467574.05F;
+            //int i1 = (int)f1;
+            int i1 = Convert.ToInt32(f1);
+            Console.WriteLine($"Explecite Conversion done: {i1}");
+        }
+    }
+}

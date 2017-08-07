324cb76d4c82d9814ec07c95426e84274948e517
Amanullah Aman <amancse.aiub@gmail.com>
2017-08-07 15:51:18 +0600

    Loop

---
+++ loop_k/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace loop_k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While loop");
+
+            int[] abc = new int[]{1,2,3,4,5};
+
+            int i = 0;
+            while (i < abc.Length)
+            {
+                Console.Write(" {0}",abc[i]);
+                i++;
+            }
+
+            Console.WriteLine("\n\aFor loop");
+
+            for(int j =0; j<abc.Length; j++){
+                Console.Write(" "+abc[j]);
+            }
+
+            Console.WriteLine("\n\aForeach loop");
+
+            foreach (int k in abc)
+            {
+                Console.Write(" "+k);
+            }
+        }
+    }
+}

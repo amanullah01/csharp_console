f7a99eb5860dfea776d1a527720ade4857ebdb84
Amanullah Aman <amancse.aiub@gmail.com>
2017-08-07 12:04:44 +0600

    Parse and TryParse

--- y_datatype_conversion/Program.cs
+++ y_datatype_conversion/Program.cs
@@ -16,6 +16,24 @@ namespace y_datatype_conversion
             //int i1 = (int)f1;
             int i1 = Convert.ToInt32(f1);
             Console.WriteLine($"Explecite Conversion done: {i1}");
+
+            // Parse and TryParse
+
+            string s ="100";
+            int i2 = int.Parse(s);
+            Console.WriteLine("Parse methode . String to int:"+i2);
+
+            //TryParse Conversion
+            Console.WriteLine("Try pass conversion");
+            string strNumber = "9987";
+            int result = 0;
+            bool IsConversionIsTrue = int.TryParse(strNumber, out result);
+
+            if(IsConversionIsTrue){
+                Console.WriteLine("Result is : "+result);
+            }else{
+                Console.WriteLine("Please enter valid number");
+            }
         }
     }
 }
